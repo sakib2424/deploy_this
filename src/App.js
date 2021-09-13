c42bed524966f030f1a7d6f2bbe7f179edc5fb77
sakib2424 <as10239@stern.nyu.edu>
2021-09-13 17:27:26 -0400

    auth added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import LoginButton from "./LoginButton";
+import Logout from "./Logout";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton></LoginButton>
+        <Logout></Logout>
+        {user ? JSON.stringify(user, null, 2) : null}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
